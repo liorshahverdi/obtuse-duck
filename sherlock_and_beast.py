36ab20d70a44b65cc6c0245b206f3319ec8cd5a5
unknown <liorshahverdi@gmail.com>
2015-08-18 17:11:59 -0400

    im coming back to this later

---
+++ sherlock_and_beast.py
@@ -0,0 +1,7 @@
+'''
+https://www.hackerrank.com/challenges/sherlock-and-the-beast
+ A 'Decent Number' has the following properties:
+ 	1. 	3,5, or both as its digits. No other digit is allowed.
+ 	2. 	Number of times 3 appears is divisible by 5.
+ 	3. 	Number of times 5 appears is divisible by 3.
+'''
