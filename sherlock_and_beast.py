4d28566cdf09a3c0d0a01a7d718069fd78a95795
unknown <liorshahverdi@gmail.com>
2015-08-22 00:55:08 -0400

    its a start

--- sherlock_and_beast.py
+++ sherlock_and_beast.py
@@ -5,3 +5,8 @@ https://www.hackerrank.com/challenges/sherlock-and-the-beast
  	2. 	Number of times 3 appears is divisible by 5.
  	3. 	Number of times 5 appears is divisible by 3.
 '''
+num_test_cases = int(raw_input())
+for i in range(num_test_cases):
+	num_str = str(raw_input())
+	if not num_str.__contains__('5') and not num_str.__contains__('3'):
+		print 'not valid'
